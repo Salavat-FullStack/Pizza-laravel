61b618e686625eb9404b041c12ad994066af3b6b
Salavat-FullStack <webfuldeveloper@gmail.com>
2025-10-24 16:02:30 +0500

    доработка блока пиццы

--- resources/views/pizza/view.blade.php
+++ resources/views/pizza/view.blade.php
@@ -10,6 +10,10 @@
         'resources/css/main.css',
         'resources/css/pizza.css',
     ])
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    
     <title>{{ $pizza['name'] }}</title>
 </head>
 <body>
@@ -18,7 +22,25 @@
         
         <div class="pizza_container">
             <div class="pizza_image"><img src="{{ asset($pizza['image']) }}" alt="image"></div>
-            <div class="pizza_information"></div>
+            <div class="pizza_information">
+                <div class="pizza_title">{{ $pizza['name'] }}</div>
+                <div class="mini_inform">
+                    <div class="pizza_size">{{ $pizza['size'] }} см,</div>
+                    <div class="pizza_calories">{{ $pizza['finelCalories'] }} ккал,</div>
+                    <div class="weight">{{ $pizza['finelWeight'] }} г,</div>
+                    <div class="thicknesses">{{ $pizza['thicknesses'][0]['thickness'] }},</div>
+                    <div class="thicknesses">{{ $pizza['quantity'] }} шт</div>
+                </div>
+                <div class="pizza_panel">
+                    <div class="panel_size">
+                        {{-- @foreach ( as )
+                            
+                        @endforeach --}}
+                        <div class="panel_btn"></div>
+                    </div>
+                    <div class="panel_thicknesses"></div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
